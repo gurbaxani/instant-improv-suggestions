d29ded06a669a55b8522bb46a3bf252991f610c6
Gurbax <gurbax@ctocat.com>
2024-02-12 03:35:12 +0530

    remove tailwind cdsand use minify for prod

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -4,16 +4,14 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>AImprov - AI-Generated Improv $suggestions</title>
-    <script src="https://cdn.tailwindcss.com"></script>
+    <title>AImprov - AI-Generated Improv Suggestions</title>
+    @vite('resources/css/app.css')
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Lexend+Mega:wght@700&family=Public+Sans:wght@700&display=swap" rel="stylesheet">
 </head>
 
 <body class="bg-yellow-400">
-
-
     {{ $slot }}
 </body>
 
