f384e4f387f493660dae33418202c9a4725f4295
Gurbax <gurbax@ctocat.com>
2024-02-11 10:08:55 +0530

    install livewire

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,10 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tailwind Fun</title>
     @vite('resources/css/app.css')
-
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lexend+Mega:wght@700&display=swap" rel="stylesheet">
 </head>
 
-<body>
+<body class="h-screen flex items-center justify-center bg-yellow-400">
 
     <h1 class="text-3xl font-bold">
         Hello world!
