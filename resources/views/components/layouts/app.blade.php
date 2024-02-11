9c07b0369b5a8c48c3306177bfc152ce0bd7b1e6
Gurbax <gurbax@ctocat.com>
2024-02-11 22:38:46 +0530

    add tailwind cds

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -4,8 +4,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tailwind Fun</title>
-    @vite('resources/css/app.css')
+    <title>AImprov - AI-Generated Improv $suggestions</title>
+    <script src="https://cdn.tailwindcss.com"></script>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Lexend+Mega:wght@700&family=Public+Sans:wght@700&display=swap" rel="stylesheet">
